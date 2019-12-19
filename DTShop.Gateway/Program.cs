4e17962ac67d7b55fae5e56054cdbc74dcc99368
nomadedge <nomad.edge@gmail.com>
2019-12-19 14:41:40 +0300

    Add project files.

---
+++ DTShop.Gateway/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Eureka;
+using Ocelot.Provider.Polly;
+
+namespace DTShop.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true,
+                            true)
+                        .AddJsonFile("ocelot.json", false, false)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureServices(s =>
+                {
+                    s.AddOcelot()
+                    .AddEureka()
+                    .AddPolly();
+                })
+                .Configure(a =>
+                {
+                    a.UseOcelot().Wait();
+                })
+                .Build();
+        }
+    }
+}
